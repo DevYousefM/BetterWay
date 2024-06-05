b27a626161040e94a2cca822f525080a7f57b903
devyousefm <devyousefm@gmail.com>
2024-06-05 12:45:17 +0300

    link ftp

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('/', [WebController::class, 'Home'])->name('home');
 Route::get('/', function () {
-    return "Done";
+    return "acs";
 })->name('home');
