f8dd400d600d93d941adff4ab7033d8017c796dd
devyousefm <devyousefm@gmail.com>
2024-06-05 12:55:24 +0300

    last test

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,7 @@ use App\Http\Controllers\Web\WebController;
 use App\Http\Controllers\Admin\Client\ClientController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [WebController::class, 'Home'])->name('home');
+// Route::get('/', [WebController::class, 'Home'])->name('home');
+Route::get('/', function () {
+    return "acs";
+})->name('home');
