4e1d371ee45a5868b143c7a6f370845f4dbe0cd2
devyousefm <devyousefm@gmail.com>
2024-08-06 22:52:49 +0300

    Logs Lines ClientPosition

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use App\Http\Controllers\Web\WebController;
 use App\Http\Controllers\Admin\Client\ClientController;
+use App\V1\Client\Client;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WebController::class, 'Home'])->name('home');
+Route::get('/testing', function () {
+    return $clients = Client::with(['referrals', 'visits' => function ($query) {
+        $query->where('ClientBrandProductStatus', 'USED');
+    }])
+        ->whereHas('referrals', function ($query) {
+            $query->whereNotNull('IDReferral');
+        })
+        ->get();
+});
