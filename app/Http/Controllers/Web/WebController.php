0fe7f11e9165c130d7b006f678ab03a6d3ce995e
devyousefm <devyousefm@gmail.com>
2024-08-06 23:32:42 +0300

    -

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use App\V1\Client\Client;
-use App\V1\Client\Position;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
@@ -16,28 +14,8 @@ use Response;
 class WebController extends Controller
 {
 
-    public function Home()
-    {
-        $Positions = Position::all();
-        foreach ($Positions as $position) {
-            Log::info("Position:{$position->PositionTitleEn}");
-
-            // Log::info("HERE: " . Client::find(344)->referrals);
-
-            $clients = Client::with(['referrals', 'visits' => function ($query) {
-                $query->where('ClientBrandProductStatus', 'USED');
-            }])
-                ->whereHas('referrals', function ($query) {
-                    $query->whereNotNull('IDReferral');
-                })
-                ->where(function ($query) use ($position) {
-                    $query->where("IDPosition", '!=', $position->IDPosition)
-                        ->orWhereNull("IDPosition");
-                })
-                ->get();
-
-            echo $clients . "\n";
-        }
-        // return view('web.index');
+    public function Home(){
+        return view('web.index');
     }
+
 }
