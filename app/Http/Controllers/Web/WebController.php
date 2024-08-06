8221a0367b31bb710fdda6deb10b835e0f82e63a
devyousefm <devyousefm@gmail.com>
2024-08-06 22:55:33 +0300

    Logs Lines ClientPosition

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\V1\Client\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
@@ -14,8 +15,15 @@ use Response;
 class WebController extends Controller
 {
 
-    public function Home(){
-        return view('web.index');
+    public function Home()
+    {
+        return $clients = Client::with(['referrals', 'visits' => function ($query) {
+            $query->where('ClientBrandProductStatus', 'USED');
+        }])
+            ->whereHas('referrals', function ($query) {
+                $query->whereNotNull('IDReferral');
+            })
+            ->get();
+        // return view('web.index');
     }
-
 }
