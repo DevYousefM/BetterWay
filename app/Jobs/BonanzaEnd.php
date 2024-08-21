ac3db8f523e3f829d1a7e99b5821eee61e6b5a76
devyousefm <devyousefm@gmail.com>
2024-08-21 21:46:48 +0300

    send notification if user get bonanza prize

--- app/Jobs/BonanzaEnd.php
+++ app/Jobs/BonanzaEnd.php
@@ -26,80 +26,79 @@ class BonanzaEnd implements ShouldQueue
 
     protected $signature = 'log:cron';
 
-    public function __construct(){
+    public function __construct() {}
 
-    }
-
-    public function handle(){
+    public function handle()
+    {
         $CurrentTime = new DateTime('now');
         $CurrentTime = $CurrentTime->format('Y-m-d H:i:s');
 
-        $Clients = Client::where("ClientStatus","ACTIVE")->where("ClientDeleted",0)->get();
-        $Bonanzas = Bonanza::where('BonanzaStatus','ACTIVE')->where("BonanzaEndTime","<",$CurrentTime)->get();
-        foreach($Clients as $Client){
+        $Clients = Client::where("ClientStatus", "ACTIVE")->where("ClientDeleted", 0)->get();
+        $Bonanzas = Bonanza::where('BonanzaStatus', 'ACTIVE')->where("BonanzaEndTime", "<", $CurrentTime)->get();
+        foreach ($Clients as $Client) {
             $IDClient = $Client->IDClient;
-            foreach($Bonanzas as $Bonanza){
-                if($Bonanza->BonanzaLeftPoints){
-                    if($Client->ClientLeftPoints < $Bonanza->BonanzaLeftPoints){
+            foreach ($Bonanzas as $Bonanza) {
+                if ($Bonanza->BonanzaLeftPoints) {
+                    if ($Client->ClientLeftPoints < $Bonanza->BonanzaLeftPoints) {
                         continue;
                     }
                 }
-                if($Bonanza->BonanzaRightPoints){
-                    if($Client->ClientRightPoints < $Bonanza->BonanzaRightPoints){
+                if ($Bonanza->BonanzaRightPoints) {
+                    if ($Client->ClientRightPoints < $Bonanza->BonanzaRightPoints) {
                         continue;
                     }
                 }
-                if($Bonanza->BonanzaTotalPoints){
-                    if($Client->ClientTotalPoints < $Bonanza->BonanzaTotalPoints){
+                if ($Bonanza->BonanzaTotalPoints) {
+                    if ($Client->ClientTotalPoints < $Bonanza->BonanzaTotalPoints) {
                         continue;
                     }
                 }
 
                 $PlanNetwork = 0;
-                if($Bonanza->BonanzaReferralNumber){
-                    $PlanNetwork = PlanNetwork::where("IDReferralClient",$IDClient)->count();
-                    if($PlanNetwork < $Bonanza->BonanzaReferralNumber){
+                if ($Bonanza->BonanzaReferralNumber) {
+                    $PlanNetwork = PlanNetwork::where("IDReferralClient", $IDClient)->count();
+                    if ($PlanNetwork < $Bonanza->BonanzaReferralNumber) {
                         continue;
                     }
                 }
-    
+
                 $ClientProductValue = 0;
-                if($Bonanza->BonanzaProductValue){
-                    $ClientBrandProduct = ClientBrandProduct::where("IDClient",$IDClient)->where("ClientBrandProductStatus","USED")->sum("ProductTotalAmount");
-                    if($ClientBrandProduct < $Bonanza->BonanzaProductValue){
+                if ($Bonanza->BonanzaProductValue) {
+                    $ClientBrandProduct = ClientBrandProduct::where("IDClient", $IDClient)->where("ClientBrandProductStatus", "USED")->sum("ProductTotalAmount");
+                    if ($ClientBrandProduct < $Bonanza->BonanzaProductValue) {
                         continue;
                     }
                     $ClientProductValue = $ClientBrandProduct;
                 }
-    
+
                 $ClientVisitNumber = 0;
                 $BrandVisit = 0;
-                $BonanzaBrands = BonanzaBrand::where("IDBonanza",$Bonanza->IDBonanza)->where("BonanzaBrandDeleted",0)->get();
-                if(count($BonanzaBrands) || $Bonanza->BonanzaVisitNumber){
-                    if($Bonanza->BonanzaVisitNumber){
-                        $ClientBrandProduct = ClientBrandProduct::where("IDClient",$IDClient)->where("ClientBrandProductStatus","USED")->count();
-                        if($ClientBrandProduct < $Bonanza->BonanzaVisitNumber){
+                $BonanzaBrands = BonanzaBrand::where("IDBonanza", $Bonanza->IDBonanza)->where("BonanzaBrandDeleted", 0)->get();
+                if (count($BonanzaBrands) || $Bonanza->BonanzaVisitNumber) {
+                    if ($Bonanza->BonanzaVisitNumber) {
+                        $ClientBrandProduct = ClientBrandProduct::where("IDClient", $IDClient)->where("ClientBrandProductStatus", "USED")->count();
+                        if ($ClientBrandProduct < $Bonanza->BonanzaVisitNumber) {
                             continue;
                         }
                         $ClientVisitNumber = $ClientBrandProduct;
                     }
-    
-                    if(count($BonanzaBrands)){
+
+                    if (count($BonanzaBrands)) {
                         $Flag = True;
-                        foreach($BonanzaBrands as $BonanzaBrand){
-                            $ClientBrandProduct = ClientBrandProduct::leftjoin("brandproducts","brandproducts.IDBrandProduct","clientbrandproducts.IDBrandProduct")->where("clientbrandproducts.IDClient",$IDClient)->where("clientbrandproducts.ClientBrandProductStatus","USED")->where("brandproducts.IDBrand",$BonanzaBrand->IDBrand)->count();
-                            if($ClientBrandProduct < $BonanzaBrand->BonanzaBrandVisitNumber){
+                        foreach ($BonanzaBrands as $BonanzaBrand) {
+                            $ClientBrandProduct = ClientBrandProduct::leftjoin("brandproducts", "brandproducts.IDBrandProduct", "clientbrandproducts.IDBrandProduct")->where("clientbrandproducts.IDClient", $IDClient)->where("clientbrandproducts.ClientBrandProductStatus", "USED")->where("brandproducts.IDBrand", $BonanzaBrand->IDBrand)->count();
+                            if ($ClientBrandProduct < $BonanzaBrand->BonanzaBrandVisitNumber) {
                                 $Flag = False;
                                 break;
                             }
                         }
-                        if(!$Flag){
+                        if (!$Flag) {
                             continue;
                         }
                         $BrandVisit = 1;
                     }
                 }
-    
+
                 $ClientBonanza = new ClientBonanza;
                 $ClientBonanza->IDBonanza = $Bonanza->IDBonanza;
                 $ClientBonanza->IDClient = $IDClient;
@@ -112,20 +111,21 @@ class BonanzaEnd implements ShouldQueue
                 $ClientBonanza->BrandVisit = $BrandVisit;
                 $ClientBonanza->save();
 
-                $BatchNumber = "#B".$ClientBonanza->IDClientBonanza;
+                $BatchNumber = "#B" . $ClientBonanza->IDClientBonanza;
                 $TimeFormat = new DateTime('now');
                 $Time = $TimeFormat->format('H');
                 $Time = $Time . $TimeFormat->format('i');
-                $BatchNumber = $BatchNumber.$Time;
-                AdjustLedger($Client,$Bonanza->BonanzaChequeValue,$Bonanza->BonanzaRewardPoints,0,0,Null,"BONANZA","WALLET","REWARD",$BatchNumber);
-
+                $BatchNumber = $BatchNumber . $Time;
+                AdjustLedger($Client, $Bonanza->BonanzaChequeValue, $Bonanza->BonanzaRewardPoints, 0, 0, Null, "BONANZA", "WALLET", "REWARD", $BatchNumber);
+                sendFirebaseNotification($Client, $Bonanza, "bonanza", 'bonanza end and you got a prize');
+                
                 $Bonanza->BonanzaStatus = "EXPIRED";
                 $Bonanza->save();
 
                 $CompanyLedger = new CompanyLedger;
                 $CompanyLedger->IDSubCategory = 22;
                 $CompanyLedger->CompanyLedgerAmount = $Bonanza->BonanzaChequeValue;
-                $CompanyLedger->CompanyLedgerDesc = "Bonanza Payment to Client ".$Client->ClientName;
+                $CompanyLedger->CompanyLedgerDesc = "Bonanza Payment to Client " . $Client->ClientName;
                 $CompanyLedger->CompanyLedgerProcess = "AUTO";
                 $CompanyLedger->CompanyLedgerType = "DEBIT";
                 $CompanyLedger->save();
